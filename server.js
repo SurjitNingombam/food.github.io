46d410e0b600b02c589d540ff420708e00a6123b
Surjit Singh <surjitsinghningombam@gmail.com>
2018-12-22 13:39:41 +0530

    make it better 3

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/app"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.listen(process.env.PORT || 3000);
 console.log("Server up on port 3000");
