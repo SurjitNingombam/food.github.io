11a43560484f07748ffcb4c1672e7866a90a4e8e
Surjit Singh <surjitsinghningombam@gmail.com>
2018-12-22 16:39:39 +0530

    make it better 6

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + "/app"));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, user_key");
   next();
 });
 
